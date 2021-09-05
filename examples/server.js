230812e242b3f55028ee53556916fc614c65b3c1
csdoker <csd758371536@qq.com>
2021-09-05 16:25:33 +0800

    feat: add feature of upload and download progress

--- examples/server.js
+++ examples/server.js
@@ -1,6 +1,8 @@
 const express = require('express')
+const path = require('path')
 const bodyParser = require('body-parser')
 const cookieParser = require('cookie-parser')
+const multipart = require('connect-multiparty')
 const webpack = require('webpack')
 const webpackDevMiddleware = require('webpack-dev-middleware')
 const webpackHotMiddleware = require('webpack-hot-middleware')
@@ -34,6 +36,12 @@ registerMoreRouter()
 app.use(bodyParser.json())
 app.use(bodyParser.urlencoded({ extended: true }))
 app.use(cookieParser())
+
+app.use(
+  multipart({
+    uploadDir: path.resolve(__dirname, 'upload-file')
+  })
+)
 
 app.use(router)
 app.use(webpackHotMiddleware(compiler))
@@ -167,5 +175,9 @@ function registerCancelRouter () {
 function registerMoreRouter () {
   router.get('/more/get', function (req, res) {
     res.json(req.cookies)
+  })
+  router.post('/more/upload', function (req, res) {
+    console.log(req.body, req.files)
+    res.end('upload success!')
   })
 }
